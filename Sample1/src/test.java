31325759b1c2de4b525ff1a27375f957687d9c9f
MR USER <MR USER@DESKTOP-9UC000P>
2020-08-30 14:17:04 +0530

    sample

---
+++ Sample1/src/test.java
@@ -0,0 +1,19 @@
+
+class c1
+{
+	void m1()
+	{
+		System.out.println("From Another class");
+	}
+}
+public class test
+{
+	public static void main(String[] ar)
+	{
+		c1 obj=new c1();
+		System.out.println("Hi welcome to all!!!");
+		obj.m1();
+		
+	}
+
+}
